4fb172d9313d4841433cce4ee81b90e2153d518b
ChenShihuan <cnchensh@outlook.com>
2020-04-24 02:36:28 +0800

    SE Net

--- pretrained_model_concatenate.py
+++ pretrained_model_concatenate.py
@@ -9,7 +9,7 @@ keras=tf.contrib.keras
 l2=keras.regularizers.l2
 K=tf.contrib.keras.backend
 import inputs as data
-from res3d_clstm_mobilenet import res3d_clstm_mobilenet
+from SEres3d_clstm_mobilenet import res3d_clstm_mobilenet
 from callbacks import LearningRateScheduler 
 from datagen import isoTrainImageGenerator, isoTestImageGenerator
 from datagen import jesterTrainImageGenerator, jesterTestImageGenerator
@@ -26,7 +26,7 @@ JESTER = 0
 ISOGD = 1
 
 cfg_modality = RGB
-cfg_dataset = JESTER
+cfg_dataset = ISOGD
 
 if cfg_modality==RGB:
   str_modality = 'rgb'
@@ -56,63 +56,42 @@ elif cfg_dataset==ISOGD:
 
 weight_decay = 0.00005
 model_prefix = './models/'
-weights_file = '%s/20epoch/%s_weights.{epoch:02d}-{val_loss:.2f}.h5'%(model_prefix,dataset_name)
-  
-_,train_labels = data.load_iso_video_list(training_datalist)
-train_steps = len(train_labels)/batch_size
-_,test_labels = data.load_iso_video_list(testing_datalist)
-test_steps = len(test_labels)/batch_size
-print 'nb_epoch: %d - seq_len: %d - batch_size: %d - weight_decay: %.6f' %(nb_epoch, seq_len, batch_size, weight_decay)
-
-def lr_polynomial_decay(global_step):
-  learning_rate = 0.001
-  end_learning_rate=0.000001
-  decay_steps=train_steps*nb_epoch
-  power = 0.9
-  p = float(global_step)/float(decay_steps)
-  lr = (learning_rate - end_learning_rate)*np.power(1-p, power)+end_learning_rate
-  return lr
 
 # load pretrained model
-num_classes = 27
 inputs = keras.layers.Input(shape=(seq_len, 112, 112, 3), batch_shape=(batch_size, seq_len, 112, 112, 3))
 feature = res3d_clstm_mobilenet(inputs, seq_len, weight_decay)
 flatten = keras.layers.Flatten(name='Flatten')(feature)
 classes = keras.layers.Dense(num_classes, activation='linear', kernel_initializer='he_normal',
                     kernel_regularizer=l2(weight_decay), name='Classes')(flatten)
 outputs = keras.layers.Activation('softmax', name='Output')(classes)
-jester_pertrained_model = keras.models.Model(inputs=inputs, outputs=outputs)
 
-pretrained_model = '%sjester_rgb_weights.04-0.71.h5'%(model_prefix)
-print 'Loading pretrained model from %s' % pretrained_model
-jester_pertrained_model.load_weights(pretrained_model, by_name=True)
+model_IsoGD_RGB  = keras.models.Model(inputs=inputs, outputs=feature)
+print(model_IsoGD_RGB.summary())
+# model_IsoGD_FLow = keras.models.Model(inputs=inputs, outputs=feature)
 
-print(jester_pertrained_model.summary())
+# IsoGD_RGB_pretrained_model  = '%sisogr_rgb_gatedclstm_weights.h5'%(model_prefix)
+# print 'Loading pretrained model from %s' % model_IsoGD_RGB
+# IsoGD_FLow_pretrained_model = '%sisogr_flow_gatedclstm_weights.h5'%(model_prefix)
+# print 'Loading pretrained model from %s' % model_IsoGD_FLow
 
-model = keras.models.Model(inputs=jester_pertrained_model.input, outputs=jester_pertrained_model.get_layer('Flatten').output)
-print(model.summary())
+# model_IsoGD_RGB.load_weights(IsoGD_RGB_pretrained_model, by_name=True)
+# print(model_IsoGD_RGB.summary())
+# # model_IsoGD_RGB = keras.models.Model(inputs=model_IsoGD_RGB.input, outputs=model_IsoGD_RGB.get_layer('Average_Pooling').output)
+# # print(model_IsoGD_RGB.summary())
 
-pretrained_model_without_full_connet = '%sjester_rgb_weights.04-0.71_pretrained.h5'%(model_prefix)
-model.save_weights(pretrained_model_without_full_connet)
-print 'Svave pretrained model without full connet layer'
+# model_IsoGD_FLow.load_weights(IsoGD_FLow_pretrained_model, by_name=True)
+# # model_IsoGD_FLow = keras.models.Model(inputs=model_IsoGD_FLow.input, outputs=model_IsoGD_FLow.get_layer('Average_Pooling').output)
+# print(model_IsoGD_FLow.summary())
 
-# if cfg_dataset==JESTER:
-#   model.fit_generator(jesterTrainImageGenerator(training_datalist, batch_size, seq_len, num_classes, cfg_modality),
-#           steps_per_epoch=train_steps,
-#           epochs=nb_epoch,
-#           verbose=1,
-#           callbacks=callbacks,
-#           validation_data=jesterTestImageGenerator(testing_datalist, batch_size, seq_len, num_classes, cfg_modality),
-#           validation_steps=test_steps,
-#           initial_epoch=init_epoch,
-#           )
-# elif cfg_dataset==ISOGD:
-#   model.fit_generator(isoTrainImageGenerator(training_datalist, batch_size, seq_len, num_classes, cfg_modality),
-#           steps_per_epoch=train_steps,
-#           epochs=nb_epoch,
-#           verbose=1,
-#           callbacks=callbacks,
-#           validation_data=isoTestImageGenerator(testing_datalist, batch_size, seq_len, num_classes, cfg_modality),
-#           validation_steps=test_steps,
-#           initial_epoch=init_epoch,
-#           )
+# r1 = model_IsoGD_RGB.output
+# r2 = model_IsoGD_FLow.output
+
+# x = keras.layers.Concatenate(axis=-1)([r1, r2])
+
+# flatten = keras.layers.Flatten(name='Flatten')(x)
+# classes = keras.layers.Dense(num_classes, activation='linear', kernel_initializer='he_normal',
+#                     kernel_regularizer=l2(weight_decay), name='Classes')(flatten)
+# outputs = keras.layers.Activation('softmax', name='Output')(classes)
+# model = keras.models.Model(inputs=inputs, outputs=outputs)
+# print(model.summary())
+
